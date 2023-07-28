879b3adaf203d953ed271f0bc7d58379f128b913
josedasilva123 <55949988+josedasilva123@users.noreply.github.com>
2023-07-28 09:24:02 -0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  const noteListData = [
+    {
+      title: "Example 1",
+      text: "Conteúdo de exemplo"
+    },
+    {
+      title: "Example 1",
+      text: "Conteúdo de exemplo"
+    }
+  ]
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
